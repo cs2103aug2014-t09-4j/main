cd4b53deb5c0210414fc142300b1887e12c2bb87
desheng <deshengg.eng+git@gmail.com>
2014-09-29 01:31:06 +0800

    Add Implementations of Database and Task
    Database allows other modules to perform CRUD on the stored Task. Basic
    functionality is completed and tested. Undo has yet to be implemented.
    
    Task contains all the methods that the user requires to manage a task, and for
    the Database to process tasks from the storage file.

---
+++ src/bakatxt/core/Database.java
@@ -0,0 +1,410 @@
+package bakatxt.core;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Database implements DatabaseInterface {
+
+    private static final String LINE_SEPARATOR = System
+            .getProperty("line.separator");
+
+    private static final String MESSAGE_FILE_CHANGE = "File changed. Current filename is \"%1$s\".";
+    private static final String MESSAGE_OUTPUT_FILENAME = "Filename: %1$s"
+            + LINE_SEPARATOR;
+    private static final String MESSAGE_OUTPUT_TOTAL_COUNT = "No. of tasks: %1$s"
+            + LINE_SEPARATOR;
+    private static final String MESSAGE_OUTPUT_DONE_COUNT = "No. of tasks completed: %1$s"
+            + LINE_SEPARATOR;
+
+    private static final String FILE_COMMENT = "[-] ";
+    private static final String FILE_HEADER = FILE_COMMENT
+            + "BakaTXT Human-Readable Human-Fixable Database";
+    private static final String FILE_VERSION = FILE_COMMENT + "alpha v0.0";
+    private static final String FILE_WARNING = FILE_COMMENT
+            + "Each task has to be in the same line";
+
+    private static final String TASK_TOTAL = "[TOTAL COUNT] ";
+    private static final String TASK_DONE = "[DONE COUNT] ";
+
+    private static final String TAG_OPEN = "[";
+    private static final String TAG_CLOSE = "]";
+    private static final String TAG_TITLE = TAG_OPEN + "TITLE" + TAG_CLOSE;
+
+    private static final int CONTENT_INDEX = 1;
+
+    private static final Charset CHARSET_DEFAULT = Charset.forName("UTF-8");
+    private static final byte[] EMPTY_BYTE = {};
+    private static final OpenOption[] OPEN_OPTIONS = {
+            StandardOpenOption.CREATE, StandardOpenOption.APPEND };
+
+    private static BigInteger taskCount;
+    private static BigInteger taskDone;
+
+    private Path _userFile;
+    private String _previousTask;
+    private BufferedWriter _outputStream;
+    private TreeMap<String, LinkedList<Task>> bakaMap;
+    private boolean _removeDone;
+
+    public Database(String fileName) {
+        setEnvironment(fileName);
+    }
+
+    private void setEnvironment(String fileName) {
+        initializeFilePath(fileName);
+        initializeOutputStream();
+        initializeVariables();
+    }
+
+    private void initializeVariables() {
+        taskCount = taskDone = BigInteger.ZERO;
+        updateMemory();
+        _previousTask = null;
+        _removeDone = false;
+    }
+
+    private void initializeFilePath(String fileName) {
+        _userFile = Paths.get(fileName);
+    }
+
+    private void initializeOutputStream() {
+        try {
+            _outputStream = Files.newBufferedWriter(_userFile, CHARSET_DEFAULT,
+                    OPEN_OPTIONS);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void updateMemory() {
+        bakaMap = new TreeMap<String, LinkedList<Task>>();
+        try (BufferedReader inputStream = Files.newBufferedReader(_userFile,
+                CHARSET_DEFAULT)) {
+            String line;
+            while ((line = inputStream.readLine()) != null) {
+                if (line.isEmpty() || line.contains("9999")
+                        || (line.contains(TASK_DONE) && _removeDone)) {
+                    continue;
+                } else if (line.contains(TAG_TITLE)) {
+                    Task task = new Task(line);
+                    addTaskToMap(task);
+                } else if (line.contains(TASK_TOTAL)) {
+                    String count = line.split(TAG_CLOSE)[CONTENT_INDEX].trim();
+                    taskCount = new BigInteger(count.trim());
+                } else if (line.contains(TASK_DONE)) {
+                    String count = line.split(TAG_CLOSE)[CONTENT_INDEX].trim();
+                    taskDone = new BigInteger(count.trim());
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void addTaskToMap(Task task) {
+        String key = task.getKey();
+
+        if (!bakaMap.containsKey(key)) {
+            bakaMap.put(key, new LinkedList<Task>());
+        }
+        LinkedList<Task> target = bakaMap.get(key);
+        target.add(task);
+    }
+
+    @Override
+    public String setFile(String fileName) {
+        setEnvironment(fileName);
+        String output = String.format(MESSAGE_FILE_CHANGE, getFileName());
+        return output;
+    }
+
+    @Override
+    public String getFileName() {
+        return _userFile.toString();
+    }
+
+    @Override
+    public String getTotalCount() {
+        return taskCount.toString();
+    }
+
+    @Override
+    public String getDoneCount() {
+        return taskDone.toString();
+    }
+
+    @Override
+    public String getUndoneCount() {
+        return taskCount.subtract(taskDone).toString();
+    }
+
+    @Override
+    public void sort() {
+        for (Map.Entry<String, LinkedList<Task>> entry : bakaMap.entrySet()) {
+            LinkedList<Task> today = entry.getValue();
+            Collections.sort(today);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String lineOne = String.format(MESSAGE_OUTPUT_FILENAME, getFileName());
+        String lineTwo = String.format(MESSAGE_OUTPUT_TOTAL_COUNT,
+                taskCount.toString());
+        String lineThree = String.format(MESSAGE_OUTPUT_DONE_COUNT,
+                taskDone.toString());
+        return lineOne + lineTwo + lineThree;
+    }
+
+    @Override
+    public Task getPrevious() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean add(Task task) {
+        task.setDeleted(false);
+        addTaskToMap(task);
+
+        if (task.isDone()) {
+            taskDone = taskDone.add(BigInteger.ONE);
+            String count = TASK_DONE + taskDone.toString();
+            dirtyWrite(count);
+        }
+
+        taskCount = taskCount.add(BigInteger.ONE);
+        String count = TASK_TOTAL + taskCount.toString();
+
+        dirtyWrite(task.toString());
+        dirtyWrite(count);
+        return updateFile();
+    }
+
+    @Override
+    public boolean delete(Task task) {
+        String key = task.getKey();
+        LinkedList<Task> target = bakaMap.get(key);
+        if (target == null) {
+            return false;
+        }
+        boolean isRemoved = target.remove(task);
+        if (isRemoved) {
+            task.setDeleted(true);
+            addTaskToMap(task);
+
+            if (task.isDone()) {
+                taskDone = taskDone.subtract(BigInteger.ONE);
+                String count = TASK_DONE + taskDone.toString();
+                dirtyWrite(count);
+            }
+
+            taskCount = taskCount.subtract(BigInteger.ONE);
+            String count = TASK_TOTAL + taskCount.toString();
+
+            dirtyWrite(task.toString());
+            dirtyWrite(count);
+        } else {
+            return isRemoved;
+        }
+        return updateFile();
+    }
+
+    @Override
+    public void close() {
+        sort();
+        updateFile();
+        try {
+            _outputStream.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private boolean updateFile() {
+        tempCreation();
+        resetFile();
+        writeFileComments();
+        writeFinalCounters();
+        return writeLinesToFile();
+    }
+
+    private void writeFileComments() {
+        try {
+            _outputStream.write(FILE_HEADER);
+            _outputStream.newLine();
+            _outputStream.write(FILE_VERSION);
+            _outputStream.newLine();
+            _outputStream.write(FILE_WARNING);
+            _outputStream.newLine();
+            _outputStream.newLine();
+            _outputStream.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void writeFinalCounters() {
+        String counter = TASK_TOTAL + taskCount.toString();
+        try {
+            _outputStream.write(counter);
+            _outputStream.newLine();
+            counter = TASK_DONE + taskDone.toString();
+            _outputStream.write(counter);
+            _outputStream.newLine();
+            _outputStream.newLine();
+            _outputStream.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private boolean writeLinesToFile() {
+        try {
+            for (Map.Entry<String, LinkedList<Task>> entry : bakaMap.entrySet()) {
+                if (_removeDone && entry.getKey().contains("5000")) {
+                    continue;
+                }
+                if (entry.getKey().contains("9999")) {
+                    continue;
+                }
+                LinkedList<Task> today = entry.getValue();
+                for (Task task : today) {
+                    _outputStream.write(task.toString());
+                    _outputStream.newLine();
+                }
+                _outputStream.flush();
+            }
+            return true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    private void resetFile() {
+        try {
+            Files.write(_userFile, EMPTY_BYTE);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void tempCreation() {
+        // copy userFile into tempFile
+        Path tempFile;
+        try {
+            tempFile = Files.createTempFile("bakatxt", "old");
+            Files.copy(_userFile, tempFile, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean setDone(Task task) {
+        delete(task);
+        task.setDone(true);
+        return add(task);
+    }
+
+    @Override
+    public LinkedList<Task> getTasks(String key) {
+        LinkedList<Task> result = new LinkedList<Task>();
+        if (key == null) {
+            for (Map.Entry<String, LinkedList<Task>> entry : bakaMap.entrySet()) {
+                if (entry.getKey().contains("0000")) {
+                    result.addAll(entry.getValue());
+                }
+            }
+        } else if (bakaMap.containsKey(key)) {
+            result = bakaMap.get(key);
+        }
+        return result;
+    }
+
+    @Override
+    public LinkedList<Task> getAllTasks() {
+        LinkedList<Task> all = new LinkedList<Task>();
+        for (Map.Entry<String, LinkedList<Task>> entry : bakaMap.entrySet()) {
+            String key = entry.getKey();
+            if (key.length() == 10 || key.contains("5000")
+                    || key.contains("0000")) {
+                LinkedList<Task> today = entry.getValue();
+                all.addAll(today);
+            }
+        }
+        return all;
+    }
+
+    @Override
+    public LinkedList<Task> getAllUndoneTasks() {
+        LinkedList<Task> undone = new LinkedList<Task>();
+        for (Map.Entry<String, LinkedList<Task>> entry : bakaMap.entrySet()) {
+            String key = entry.getKey();
+            if (!key.contains("5000") && !key.contains("9999")) {
+                LinkedList<Task> today = entry.getValue();
+                undone.addAll(today);
+            }
+        }
+        return undone;
+    }
+
+    @Override
+    public boolean setFloating(Task task) {
+        delete(task);
+        task.setFloating(true);
+        add(task);
+        return updateFile();
+    }
+
+    private void dirtyWrite(String line) {
+        try {
+            _outputStream.write(line);
+            _outputStream.newLine();
+            _outputStream.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void resetCounters() {
+        taskCount = BigInteger.ZERO;
+        taskDone = BigInteger.ZERO;
+        for (Map.Entry<String, LinkedList<Task>> entry : bakaMap.entrySet()) {
+            String key = entry.getKey();
+            if (!key.contains("9999")) {
+                String size = String.valueOf(entry.getValue().size());
+                taskCount = taskCount.add(new BigInteger(size));
+            }
+            if (key.contains("5000")) {
+                String size = String.valueOf(entry.getValue().size());
+                taskDone = taskDone.add(new BigInteger(size));
+            }
+        }
+    }
+
+    @Override
+    public void removeDone() {
+        taskCount = taskCount.subtract(taskDone);
+        taskDone = BigInteger.ZERO;
+        _removeDone = true;
+        updateFile();
+        updateMemory();
+        _removeDone = false;
+    }
+
+}
---
+++ src/bakatxt/core/Task.java
@@ -0,0 +1,323 @@
+package bakatxt.core;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task implements TaskInterface, Comparable<Task> {
+
+    private static final String SPACE = " ";
+    private static final String LINE_SEPARATOR = System
+            .getProperty("line.separator");
+
+    private static final String MESSAGE_NO_DATE = "No date is specified for this task.";
+    private static final String MESSAGE_NO_TIME = "No time is specified for this task.";
+    private static final String MESSAGE_NO_VENUE = "No venue is specified for this task";
+    private static final String MESSAGE_NO_DESCRIPTION = "No description is specified for this task";
+
+    private static final String TAG_TAB = "\t";
+    private static final String TAG_OPEN = "[";
+    private static final String TAG_CLOSE = "]";
+    private static final String TAG_TITLE = TAG_OPEN + "TITLE" + TAG_CLOSE;
+    private static final String TAG_DATE = TAG_OPEN + "DATE" + TAG_CLOSE;
+    private static final String TAG_TIME = TAG_OPEN + "TIME" + TAG_CLOSE;
+    private static final String TAG_VENUE = TAG_OPEN + "VENUE" + TAG_CLOSE;
+    private static final String TAG_DESCRIPTION = TAG_OPEN + "DESCRIPTION"
+            + TAG_CLOSE;
+    private static final String TAG_FLOATING = TAG_OPEN + "FLOATING"
+            + TAG_CLOSE;
+    private static final String TAG_DONE = TAG_OPEN + "DONE" + TAG_CLOSE;
+    private static final String TAG_DELETED = TAG_OPEN + "DELETED" + TAG_CLOSE;
+
+    private static final String TAG_TRUE = "true";
+    private static final String TAG_NULL = "null";
+
+    private static final String TAG_FLOATING_HEAD = "0000" + SPACE;
+    private static final String TAG_DONE_HEAD = "5000" + SPACE;
+    private static final String TAG_DELETED_HEAD = "9999" + SPACE;
+
+    private String _title;
+    private String _date;
+    private String _time;
+    private String _venue;
+    private String _description; // TODO allow line breaks for description
+    private boolean _isFloating;
+    private boolean _isDone;
+    private boolean _isDeleted;
+
+    public Task() {
+        _title = null;
+        _date = null;
+        _time = null;
+        _venue = null;
+        _description = null;
+        _isDone = false;
+        _isFloating = false;
+        _isDeleted = false;
+    }
+
+    public Task(String input) {
+        List<String> tokenizedInput = Arrays.asList(input.split("\\s+"));
+        if (!tokenizedInput.contains(TAG_TITLE)) {
+            _title = input;
+            _date = null;
+            _time = null;
+            _venue = null;
+            _description = null;
+            _isDone = false;
+            _isFloating = false;
+            _isDeleted = false;
+        } else if (isValidTaskString(input)) {
+            // TODO fix semi corrupted tasks
+            initializeFromDatabaseString(tokenizedInput);
+        }
+    }
+
+    private static boolean isValidTaskString(String input) {
+        ArrayList<String> headers = new ArrayList<String>();
+        headers.add(TAG_TITLE);
+        headers.add(TAG_DATE);
+        headers.add(TAG_TIME);
+        headers.add(TAG_VENUE);
+        headers.add(TAG_DONE);
+        headers.add(TAG_FLOATING);
+        headers.add(TAG_DELETED);
+        headers.add(TAG_DESCRIPTION);
+
+        for (String header : headers) {
+            if (!input.contains(header)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void initializeFromDatabaseString(List<String> tokenizedInput) {
+        int titleIndex = tokenizedInput.indexOf(TAG_TITLE) + 1;
+        StringBuilder title = new StringBuilder();
+        for (int i = titleIndex; i < tokenizedInput.indexOf(TAG_DATE); i++) {
+            title.append(tokenizedInput.get(i));
+            title.append(SPACE);
+        }
+        _title = title.toString().trim();
+
+        int dateIndex = tokenizedInput.indexOf(TAG_DATE) + 1;
+        _date = tokenizedInput.get(dateIndex);
+
+        int timeIndex = tokenizedInput.indexOf(TAG_TIME) + 1;
+        _time = tokenizedInput.get(timeIndex);
+
+        int venueIndex = tokenizedInput.indexOf(TAG_VENUE) + 1;
+        _venue = tokenizedInput.get(venueIndex);
+
+        int doneIndex = tokenizedInput.indexOf(TAG_DONE) + 1;
+        _isDone = tokenizedInput.get(doneIndex).equals(TAG_TRUE);
+
+        int floatingIndex = tokenizedInput.indexOf(TAG_FLOATING) + 1;
+        _isFloating = tokenizedInput.get(floatingIndex).equals(TAG_TRUE);
+
+        int deletedIndex = tokenizedInput.indexOf(TAG_DELETED) + 1;
+        _isDeleted = tokenizedInput.get(deletedIndex).equals(TAG_TRUE);
+
+        int descriptionIndex = tokenizedInput.indexOf(TAG_DESCRIPTION) + 1;
+        if (descriptionIndex + 1 == tokenizedInput.size()) {
+            if (tokenizedInput.get(descriptionIndex).equals(TAG_NULL)) {
+                _description = null;
+            }
+        } else {
+            StringBuilder description = new StringBuilder();
+            for (int i = descriptionIndex; i < tokenizedInput.size(); i++) {
+                description.append(tokenizedInput.get(i));
+                description.append(SPACE);
+            }
+            _description = description.toString().trim();
+        }
+    }
+
+    @Override
+    public String getTitle() {
+        return _title;
+    }
+
+    @Override
+    public String getDate() {
+        if (_date == null) {
+            return MESSAGE_NO_DATE;
+        }
+        return _date;
+    }
+
+    @Override
+    public String getTime() {
+        if (_time == null) {
+            return MESSAGE_NO_TIME;
+        }
+        return _time;
+    }
+
+    @Override
+    public String getVenue() {
+        if (_venue == null) {
+            return MESSAGE_NO_VENUE;
+        }
+        return _venue;
+    }
+
+    @Override
+    public String getDescription() {
+        if (_description == null) {
+            return MESSAGE_NO_DESCRIPTION;
+        }
+        return _description;
+    }
+
+    @Override
+    public String addTitle(String input) {
+        _title = input.trim();
+        return _title;
+    }
+
+    @Override
+    public String addDate(String input) {
+        _date = input.trim();
+        return _date;
+    }
+
+    @Override
+    public String addTime(String input) {
+        _time = input.trim();
+        return _time;
+    }
+
+    @Override
+    public String addVenue(String input) {
+        _venue = input.trim();
+        return _venue;
+    }
+
+    @Override
+    public String addDescription(String input) {
+        _description = input.trim();
+        return _description;
+    }
+
+    @Override
+    public boolean isDone() {
+        return _isDone;
+    }
+
+    @Override
+    public String toDisplayString() {
+        StringBuilder task = new StringBuilder();
+
+        task.append(TAG_OPEN + _date + SPACE + _time + TAG_CLOSE + SPACE
+                + LINE_SEPARATOR);
+        task.append(TAG_TAB + TAG_TITLE + SPACE + _title + SPACE
+                + LINE_SEPARATOR);
+        task.append(TAG_TAB + TAG_DATE + SPACE + _date + SPACE + LINE_SEPARATOR);
+        task.append(TAG_TAB + TAG_TIME + SPACE + _time + SPACE + LINE_SEPARATOR);
+        task.append(TAG_TAB + TAG_VENUE + SPACE + _venue + SPACE
+                + LINE_SEPARATOR);
+        task.append(TAG_TAB + TAG_DESCRIPTION + SPACE + _description + SPACE
+                + LINE_SEPARATOR);
+        task.append(TAG_TAB + TAG_DONE + SPACE + _isDone + SPACE
+                + LINE_SEPARATOR);
+        task.append(TAG_TAB + TAG_FLOATING + SPACE + _isFloating + SPACE
+                + LINE_SEPARATOR);
+
+        return task.toString();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder task = new StringBuilder();
+
+        task.append(TAG_OPEN);
+
+        if (_isDeleted) {
+            task.append(TAG_DELETED_HEAD);
+        }
+
+        if (_isDone) {
+            task.append(TAG_DONE_HEAD);
+        }
+
+        if (_isFloating) {
+            task.append(TAG_FLOATING_HEAD);
+        }
+
+        task.append(_date + SPACE + _time + TAG_CLOSE + SPACE);
+        task.append(TAG_TITLE + SPACE + _title + SPACE);
+        task.append(TAG_DATE + SPACE + _date + SPACE);
+        task.append(TAG_TIME + SPACE + _time + SPACE);
+        task.append(TAG_VENUE + SPACE + _venue + SPACE);
+        task.append(TAG_DONE + SPACE + _isDone + SPACE);
+        task.append(TAG_FLOATING + SPACE + _isFloating + SPACE);
+        task.append(TAG_DELETED + SPACE + _isDeleted + SPACE);
+        task.append(TAG_DESCRIPTION + SPACE + _description + SPACE);
+
+        return task.toString();
+    }
+
+    @Override
+    public void setDeleted(boolean isDeleted) {
+        _isDeleted = isDeleted;
+    }
+
+    @Override
+    public boolean isFloating() {
+        return _isFloating;
+    }
+
+    @Override
+    public void setDone(boolean isDone) {
+        _isDone = isDone;
+    }
+
+    @Override
+    public void setFloating(boolean isFloating) {
+        _isFloating = isFloating;
+    }
+
+    @Override
+    public boolean isDeleted() {
+        return _isDeleted;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Task)) {
+            return false;
+        }
+        Task task = (Task) obj;
+        return this.toString().equals(task.toString());
+    }
+
+    @Override
+    public int compareTo(Task task) {
+        String thisOne = getKey() + getTime();
+        String thatOne = task.getKey() + getTime();
+        return thisOne.compareTo(thatOne);
+    }
+
+    @Override
+    public String getKey() {
+        StringBuilder key = new StringBuilder();
+
+        if (_isDeleted) {
+            key.append(TAG_DELETED_HEAD);
+        }
+
+        if (_isDone) {
+            key.append(TAG_DONE_HEAD);
+        }
+
+        if (_isFloating) {
+            key.append(TAG_FLOATING_HEAD);
+        }
+
+        key.append(_date);
+
+        return key.toString();
+    }
+}
