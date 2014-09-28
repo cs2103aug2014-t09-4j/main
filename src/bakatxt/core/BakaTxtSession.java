175bdfafa53439706fb2d963fa4e57532894fbb8
desheng <deshengg.eng+git@gmail.com>
2014-09-29 01:26:41 +0800

    Add skeleton file for BakaTxtSession

---
+++ src/bakatxt/core/BakaTxtSession.java
@@ -0,0 +1,95 @@
+package bakatxt.core;
+public class BakaTxtSession implements BakaTxtSessionInterface {
+
+    // TODO Ensure filename input is correct
+    // TODO Ensure database is closed
+
+    public BakaTxtSession(String fileName) {
+        // TODO set up database
+    }
+
+    @Override
+    public String add(String input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String delete(String input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String display(String input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String display() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void sort() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void exit() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getFileName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getPrevious() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getTotal() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getUndone() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getDone() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String done(String input) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getTask(String title) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String deleteDone() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
