9554cc6a50b7631f7e10cca02b4ef4ffd91e1c97
Bjorn Lim <mail@bjornl.im>
2014-10-06 11:30:20 +0800

    Slightly modified layout

--- src/bakatxt/gui/Contents.java
+++ src/bakatxt/gui/Contents.java
@@ -9,9 +9,7 @@ import javax.swing.JPanel;
 
 class Contents extends JPanel {
 
-    private Input input_;
-
-    public Contents(Input input) {
+    public Contents() {
         setOpaque(false);
         setBackground(UIHelper.TRANSPARENT);
         setLayout(new GridBagLayout());
@@ -21,15 +19,16 @@ class Contents extends JPanel {
     private void addComponentsToPane() {
         GridBagConstraints layout = new GridBagConstraints();
 
-        setLeftTime(layout);
-        setRightTime(layout);
+        setAlertMessage(layout);
+        setDateAndDay(layout);
         setEvents(layout);
     }
 
-    private void setLeftTime(GridBagConstraints layout) {
-        FormattedText time = new FormattedText("<Day>", Font.PLAIN, 12, UIHelper.PRESET_COLOR_DATE);
+    private void setAlertMessage(GridBagConstraints layout) {
+        // TODO set message
+        FormattedText time = new FormattedText("<Message>", Font.PLAIN, 12, UIHelper.PRESET_COLOR_DATE);
         layout.fill = GridBagConstraints.NONE;
-        layout.anchor = GridBagConstraints.FIRST_LINE_START;
+        layout.anchor = GridBagConstraints.PAGE_START;
         layout.weightx = 0.0;
         layout.weighty = 0.01;
         layout.gridx = 0;
@@ -39,27 +38,28 @@ class Contents extends JPanel {
         this.add(time, layout);
     }
 
-    private void setRightTime(GridBagConstraints layout) {
-        FormattedText time = new FormattedText("<Time>", Font.PLAIN, 12, UIHelper.PRESET_COLOR_DATE);
+    private void setDateAndDay(GridBagConstraints layout) {
+        // TODO set time and day
+        FormattedText time = new FormattedText("<Day><Date>", Font.PLAIN, 12, UIHelper.PRESET_COLOR_DATE);
         layout.fill = GridBagConstraints.NONE;
-        layout.anchor = GridBagConstraints.FIRST_LINE_END;
+        layout.anchor = GridBagConstraints.LAST_LINE_END;
         layout.weightx = 0.0;
         layout.weighty = 0.01;
-        layout.gridx = 1;
-        layout.gridy = 0;
+        layout.gridx = 0;
+        layout.gridy = 1;
         layout.gridheight = 1;
         layout.gridwidth = 1;
         this.add(time, layout);
     }
 
     private void setEvents(GridBagConstraints layout) {
-        Tasks events = new Tasks(input_);
+        Tasks events = new Tasks();
         layout.fill = GridBagConstraints.BOTH;
         layout.anchor = GridBagConstraints.FIRST_LINE_START;
         layout.weightx = 1.0;
         layout.weighty = 1.0;
         layout.gridx = 0;
-        layout.gridy = 1;
+        layout.gridy = 2;
         layout.gridheight = 1;
         layout.gridwidth = GridBagConstraints.REMAINDER;
         layout.insets = new Insets(UIHelper.WINDOW_BORDER, 0, 0, 0);
