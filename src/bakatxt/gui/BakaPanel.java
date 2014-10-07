2081e8d9c2eae29b81928d543966d64f35304d6a
Bjorn Lim <mail@bjornl.im>
2014-10-07 18:33:13 +0800

    Cleaned up and commented BakaPanel. Also reduced the access rights of some methods

--- src/bakatxt/gui/BakaPanel.java
+++ src/bakatxt/gui/BakaPanel.java
@@ -12,12 +12,16 @@ import javax.swing.JPanel;
 
 import bakatxt.core.BakaTxtMain;
 
-// TODO comments
+/**
+ * This class is used as the actual window (i.e, what the user perceives as a window)
+ * for BakaTXT. Apart from that, it also draws the input box and the rest of the
+ * content.
+ *
+ */
 class BakaPanel extends JPanel {
 
     private static Input _input;
     private static Contents _contents;
-    private static final int BORDER = UIHelper.WINDOW_BORDER;
 
     public BakaPanel(BakaTxtMain session) {
 
@@ -31,19 +35,30 @@ class BakaPanel extends JPanel {
         addComponentsToPane();
     }
 
-    public Input getInput() {
+    /**
+     * @return the input box
+     */
+    protected Input getInput() {
         return _input;
     }
 
-    public void setContents(BakaTxtMain session) {
+    /**
+     * This method refreshes what is displayed on the GUI
+     *
+     * @param session is the logic module we are retrieving the new information from
+     */
+    protected void setContents(BakaTxtMain session) {
 
          _contents.removeAll();
          _contents.updateContents(session.getAllTasks());
          _contents.validate();
          _contents.repaint();
-
     }
 
+    /**
+     * This method dictates the area where the input box and the content is allowed
+     * to be
+     */
     private void addComponentsToPane() {
         GridBagConstraints layout = new GridBagConstraints();
 
@@ -51,6 +66,11 @@ class BakaPanel extends JPanel {
         setContent(layout);
     }
 
+    /**
+     * Tell the GridBag where to draw the input box
+     *
+     * @param layout is the GridBag which is being drawn on
+     */
     private void setInputBox(GridBagConstraints layout) {
         layout.fill = GridBagConstraints.BOTH;
         layout.anchor = GridBagConstraints.FIRST_LINE_START;
@@ -58,10 +78,16 @@ class BakaPanel extends JPanel {
         layout.weighty = 0.03;
         layout.gridx = 0;
         layout.gridy = 0;
-        layout.insets = new Insets(2 * BORDER, 2 * BORDER, BORDER, 2 * BORDER);
+        layout.insets = new Insets(2 * UIHelper.WINDOW_BORDER, 2 * UIHelper.WINDOW_BORDER,
+                UIHelper.WINDOW_BORDER, 2 * UIHelper.WINDOW_BORDER);
         this.add(_input, layout);
     }
 
+    /**
+     * Tell the GridBag where to draw the content box
+     *
+     * @param layout is the GridBag which is being drawn on
+     */
     private void setContent(GridBagConstraints layout) {
         layout.fill = GridBagConstraints.BOTH;
         layout.anchor = GridBagConstraints.FIRST_LINE_START;
@@ -69,10 +95,14 @@ class BakaPanel extends JPanel {
         layout.weighty = 1.0;
         layout.gridx = 0;
         layout.gridy = 1;
-        layout.insets = new Insets(0, 2 * BORDER, 2 * BORDER, 2 * BORDER);
+        layout.insets = new Insets(0, 2 * UIHelper.WINDOW_BORDER,
+                2 * UIHelper.WINDOW_BORDER, 2 * UIHelper.WINDOW_BORDER);
         this.add(_contents, layout);
     }
 
+    /**
+     * Make the corners of the BakaPanel round
+     */
     @Override
     protected void paintComponent(Graphics g) {
         UIHelper.paintRoundedRectangle(g, getBackground(), getWidth(), getHeight());
