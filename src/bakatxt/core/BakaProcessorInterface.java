5c4e510002a0c3c36bdee1cd7b6d7d67afbb17c5
desheng <deshengg.eng+git@gmail.com>
2014-10-16 23:02:38 +0800

    Update BakaProcessorInterface to reflect the IOException removal

--- src/bakatxt/core/BakaProcessorInterface.java
+++ src/bakatxt/core/BakaProcessorInterface.java
@@ -1,22 +1,21 @@
 package bakatxt.core;
 
-import java.io.IOException;
 import java.util.LinkedList;
 
 public interface BakaProcessorInterface {
-    public String executeCommand(String input) throws IOException;
+    public String executeCommand(String input);
 
     public String addTask(String input, String output);
 
-    public void deleteTask(String input) throws IOException;
+    public void deleteTask(String input);
 
     public void displayTask();
 
-    public void editTask(String input) throws IOException;
+    public void editTask(String input);
 
     public LinkedList<Task> getAllTasks();
 
-    public void clearTask() throws IOException;
+    public void clearTask();
 
-    public void exitProg() throws IOException;
+    public void exitProg();
 }
