da0bc9b80b19ac756d45edc4507261b93ae40b7d
Ki Hyun <gilkihyun@gmail.com>
2014-10-22 23:40:35 +0800

    Add JUnit test cases for command type under BakaProcessor

--- src/bakatxt/test/BakaProcessorTest.java
+++ src/bakatxt/test/BakaProcessorTest.java
@@ -1,19 +1,51 @@
 package bakatxt.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
-public class BakaProcessorTest {
+import bakatxt.core.BakaParser;
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
+public class BakaProcessorTest {
+    private BakaParser _parser;
+
+    enum CommandType {
+        ADD, DELETE, EDIT, EXIT, DISPLAY, UNDO, REDO, DEFAULT
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        _parser = new BakaParser();
     }
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testCommandType() {
+        String input = "add lunch";
+        String command = _parser.getCommand(input);
+        CommandType commandType = CommandType.valueOf(command);
+        assertEquals("ADD", commandType);
     }
 
+    @Test
+    public void testCommandType2() {
+        String input = "delete 3";
+        String command = _parser.getCommand(input);
+        CommandType commandType = CommandType.valueOf(command);
+        assertEquals("DELETE", commandType);
+    }
+
+    @Test
+    public void testWrongCommandType() {
+        String input = "remove 3";
+        String command = _parser.getCommand(input);
+        CommandType commandType;
+        try {
+            commandType = CommandType.valueOf(command);
+        } catch (IllegalArgumentException e) {
+            commandType = CommandType.DEFAULT;
+        }
+        assertEquals("DEFAULT", commandType);
+
+    }
 }
