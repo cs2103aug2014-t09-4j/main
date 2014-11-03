0b2a60ca12f2feaa3b0b973219421338425df5ef
Bjorn Lim <mail@bjornl.im>
2014-11-03 19:43:08 +0800

    Resolve #1

--- src/bakatxt/gui/TaskBox.java
+++ src/bakatxt/gui/TaskBox.java
@@ -26,6 +26,11 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
     protected static final boolean IS_LINE_WRAP = true;
     private static final String AT = " @";
 
+    private static final String DONE = "✓";
+    private static final String UNDONE = "✗";
+    private static final String OVERDUE = UNDONE + "!";
+    private static final String NO_TIME = "        ";
+
     private final Color _baseColor;
 
     public TaskBox(Task task, int index, Color backgroundColor) {
@@ -45,6 +50,7 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
         setTaskAndLocation(layout, task.getTitle(), task.getVenue());
         setTimeStart(layout, task.getTime());
         setDescription(layout, task.getDescription());
+        setTaskCompletionState(layout, doneState(task));
         setTimeEnd(layout, task.getEndTime());
     }
 
@@ -53,7 +59,7 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
         FormattedText index = new FormattedText(shouldAddLeadingZero(number),
                                                 ThemeReader.getNumberTheme());
         layout.fill = GridBagConstraints.NONE;
-        layout.anchor = GridBagConstraints.CENTER;
+        layout.anchor = GridBagConstraints.LAST_LINE_START;
         layout.weightx = 1.0;
         layout.weighty = 0.0;
         layout.gridx = 0;
@@ -67,12 +73,12 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
     // TODO make locationText a different color
     private void setTaskAndLocation(GridBagConstraints layout, String taskText,
                                     String locationText) {
-        locationText = removeNullValues(AT, locationText);
+        locationText = removeNullValuesLocation(AT, locationText);
         FormattedText task = new FormattedText(taskText + locationText,
                                                ThemeReader.getTitleTheme(),
                                                IS_LINE_WRAP);
         layout.fill = GridBagConstraints.HORIZONTAL;
-        layout.anchor = GridBagConstraints.LINE_START;
+        layout.anchor = GridBagConstraints.LAST_LINE_START;
         layout.weightx = 0.01;
         layout.weighty = 0.0;
         layout.gridx = 1;
@@ -88,20 +94,20 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
                                                       ThemeReader.getDefaultTheme(),
                                                       IS_LINE_WRAP);
         layout.fill = GridBagConstraints.BOTH;
-        layout.anchor = GridBagConstraints.FIRST_LINE_START;
+        layout.anchor = GridBagConstraints.LINE_START;
         layout.weightx = 1.0;
         layout.weighty = 1.0;
         layout.gridx = 1;
         layout.gridy = 1;
         layout.gridwidth = 1;
-        layout.gridheight = 1;
+        layout.gridheight = 2;
         layout.insets = new Insets(0, 4 * UIHelper.BORDER, 0, 0);
         this.add(description, layout);
     }
 
     private void setTimeStart(GridBagConstraints layout, String startTimeText) {
 
-        startTimeText = removeNullValues(startTimeText);
+        startTimeText = removeNullValuesTime(startTimeText);
         FormattedText time = new FormattedText(startTimeText,
                                                ThemeReader.getDateTheme());
         layout.fill = GridBagConstraints.NONE;
@@ -117,9 +123,24 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
         this.add(time, layout);
     }
 
+    private void setTaskCompletionState(GridBagConstraints layout, String state) {
+        FormattedText completionState = new FormattedText(state,
+                                                          ThemeReader.getLocationTheme());
+        layout.fill = GridBagConstraints.NONE;
+        layout.anchor = GridBagConstraints.CENTER;
+        layout.weightx = 0.01;
+        layout.weighty = 1.0;
+        layout.gridx = 2;
+        layout.gridy = 1;
+        layout.gridwidth = 1;
+        layout.gridheight = 1;
+        layout.insets = new Insets(0, 0, 0, 2 * UIHelper.BORDER);
+        this.add(completionState, layout);
+    }
+
     private void setTimeEnd(GridBagConstraints layout, String endTimeText) {
 
-        endTimeText = removeNullValues(endTimeText);
+        endTimeText = removeNullValuesTime(endTimeText);
         FormattedText time = new FormattedText(endTimeText,
                                                ThemeReader.getDateTheme());
         layout.fill = GridBagConstraints.NONE;
@@ -127,7 +148,7 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
         layout.weightx = 0.01;
         layout.weighty = 1.0;
         layout.gridx = 2;
-        layout.gridy = 1;
+        layout.gridy = 2;
         layout.gridwidth = 1;
         layout.gridheight = 1;
         layout.insets = new Insets(0, 0, 3 * UIHelper.BORDER,
@@ -135,15 +156,15 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
         this.add(time, layout);
     }
 
-    private static String removeNullValues(String s) {
-        if (s == null || s.equals("null")) {
-            return "";
+    private static String removeNullValuesTime(String s) {
+        if (isStringNull(s)) {
+            return NO_TIME;
         }
         return s;
     }
 
-    private static String removeNullValues(String prefix, String s) {
-        if (s == null || s.equals("null")) {
+    private static String removeNullValuesLocation(String prefix, String s) {
+        if (isStringNull(s)) {
             return "";
         }
         return prefix + s;
@@ -155,6 +176,19 @@ abstract class TaskBox extends JPanel implements BakaAnimator {
             num = "0" + num;
         }
         return num;
+    }
+
+    private static boolean isStringNull(String s) {
+        return (s == null || s.equals("null"));
+    }
+
+    private static String doneState(Task task) {
+        if (task.isDone()) {
+            return DONE;
+        } else if (task.isOverdue()) {
+            return OVERDUE;
+        }
+        return UNDONE;
     }
 
     @Override
