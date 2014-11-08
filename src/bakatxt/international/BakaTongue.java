e20cad0ecad8403da0d37bc3b973e8a966d5c167
desheng <deshengg.eng+git@gmail.com>
2014-11-08 15:41:09 +0800

    Add comments for BakaTongue

--- src/bakatxt/international/BakaTongue.java
+++ src/bakatxt/international/BakaTongue.java
@@ -21,9 +21,14 @@ public class BakaTongue {
     private static ResourceBundle resBundle = ResourceBundle.getBundle(
             BUNDLE_NAME, currentLocale);
 
-    private BakaTongue() {
-    }
-
+    /**
+     * Retrieves the information from the specified resource bundle
+     * 
+     * @param key
+     *            of the <code>String</code> to be retrieved
+     * @return <code>String</code> from the resource bundle, or
+     *         <code>!KEY!</code> if the key does not exists.
+     */
     public static String getString(String key) {
         try {
             return resBundle.getString(key);
@@ -32,6 +37,16 @@ public class BakaTongue {
         }
     }
 
+    /**
+     * Sets the resource bundle according to the language specified.
+     * 
+     * @param language
+     *            <code>String</code> chosen by the user
+     * 
+     * @return <code>true</code> if the language specified exists and is
+     *         applied, <code>false</code> when language does not exist and
+     *         default English is applied.
+     */
     public static boolean setLanguage(String language) {
         boolean isSuccessful = true;
         switch (language.toLowerCase().trim()) {
@@ -66,11 +81,30 @@ public class BakaTongue {
         return isSuccessful;
     }
 
+    /**
+     * Enable setting of language directly based on specifying locale and
+     * region. Used to set language from preferences stored in the storage file.
+     * 
+     * @param lang
+     *            <code>String</code> containing locale language code
+     * @param region
+     *            <code>String</code> containing locale region code
+     */
     public static void setLanguage(String lang, String region) {
         currentLocale = new Locale(lang.trim(), region.trim());
         resBundle = ResourceBundle.getBundle(BUNDLE_NAME, currentLocale);
     }
 
+    /**
+     * Enables pseudo translation of user commands by direct translation of user
+     * command input from other languages to English. Only replaces identifying
+     * elements such as commands and delimiters.
+     * 
+     * @param input
+     *            <code>String</code> containing the user command
+     * @return <code>String</code> containing the original input with replaced
+     *         <code>String</code> of identifiers.
+     */
     public static String toEnglish(String input) {
         Set<String> keys = resBundle.keySet();
 
@@ -104,16 +138,46 @@ public class BakaTongue {
         return input;
     }
 
+    /**
+     * Checks if the command identified is in English.
+     * 
+     * @param input
+     *            from the user containing the Command <code>String</code>
+     * @param international
+     *            Command from the English resource bundle
+     * @param commandLength
+     *            <code>length</code> of the Command <code>String</code>
+     * 
+     * @return <code>true</code> if the Commands are equal, <code>false</code>
+     *         otherwise.
+     */
     private static boolean isSameCommand(String input, String international,
             int commandLength) {
         return input.substring(0, commandLength).toUpperCase()
                 .equals(international);
     }
 
+    /**
+     * Checks if the input is of sufficient length
+     * 
+     * @param input
+     *            to be checked
+     * @param commandLength
+     *            length of the command to be checked against
+     * 
+     * @return <code>true</code> if acceptable, <code>false</code> otherwise
+     */
     private static boolean isAcceptable(String input, int commandLength) {
         return input.length() >= commandLength;
     }
 
+    /**
+     * Packages the languages into a <code>LinkedList</code> to be displayed to
+     * the user as choices.
+     * 
+     * @return <code>LinkedList</code> containing pseudo-Tasks with information
+     *         about the languages.
+     */
     public static LinkedList<Task> languageChoices() {
         LinkedList<Task> choices = new LinkedList<Task>();
         for (int i=0; i<LANGUAGES.length; i++) {
